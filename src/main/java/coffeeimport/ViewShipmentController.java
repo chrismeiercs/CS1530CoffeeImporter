869faea66ece74eeb10de68df7f7154f52d6d2ac
chrismeiercs <chris.g.meier@gmail.com>
2015-07-04 01:01:28 -0400

    User Story 7 Complete

---
+++ src/main/java/coffeeimport/ViewShipmentController.java
@@ -0,0 +1,20 @@
+package coffeeimport;/**
+ * Created by Chris on 7/4/2015.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shipmentviewer")
+public class ViewShipmentController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(Model model) {
+
+        return "ViewShipments";
+    }
+
+}
